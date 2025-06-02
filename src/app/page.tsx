2862a05cd667e3ac79da98a4f65417b9086e39c8
narthkings <imohdumto@gmail.com>
2025-06-02 01:24:29 +0100

    feat: check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,6 +68,7 @@ export default function Home() {
         </select>
       </div>
 
+
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
         {
           fetchingSearchedCountry || fetchingFilteredCountry || fetchingAllCountries
